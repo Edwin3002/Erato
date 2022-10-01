92702291a7f7b1768cb58e82ae4074de6b0eca1f
Edwin3002 <vargas11742@gmail.com>
2022-09-30 20:35:54 -0500

    logo

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,14 @@
 import Footer from '../components/footer/Footer'
 import Navb from '../components/navbar/Navb'
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-    {/* <Head></Head> */}
+    <Head>
+    <link rel="shortcut icon" href="https://res.cloudinary.com/edwin3002/image/upload/v1664576092/Erato/logo-1_fyxulh.png" />
+    </Head>
       <Navb/>
       <Component {...pageProps} />
       <Footer/>
