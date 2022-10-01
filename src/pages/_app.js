20e0abe087bcb7fbb85a2f502e6aaf27e809cbfb
Edwin3002 <vargas11742@gmail.com>
2022-09-30 19:08:03 -0500

    index

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,13 @@
+import Navb from '../components/navbar/Navb'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navb/>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,30 @@
 
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div >
-      <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-          <h1 className='underline text-center'>hola</h1>
+      <article className='md:flex w-full h-screen'>
+        <section className='w-full md:w-3/4 h-1/2 md:h-3/4 p-4'>
+          <iframe className='w-full min-h-[350px] md:min-h-[700px] rounded-lg' src="https://www.youtube.com/embed/ARDaOC7Pxmo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </section>
+        <section className='w-full md:w-1/4 h-5/6'>
+          <h1 className='text-center text-4xl text-bold my-4'>Lista de cursos</h1>
+          <ul className=' flex flex-col justify-around h-3/4'>
+            <li className='flex'>
+              <span className='bg-red-400 rounded px-10 py-2 m-auto'>Curso 1</span>
+            </li>
+            <li className='flex'>
+              <span className='bg-red-400 rounded px-10 py-2 m-auto'>Curso 2</span>
+            </li>
+            <li className='flex'>
+              <span className='bg-red-400 rounded px-10 py-2 m-auto'>Curso 3</span>
+            </li>
+            <li className='flex'>
+              <span className='bg-red-400 rounded px-10 py-2 m-auto'>Curso 4</span>
+            </li>
+          </ul>
+        </section>
+      </article>
     </div>
   )
 }
