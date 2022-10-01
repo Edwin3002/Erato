6f4aeef3c0cb8847b91ba7a69a7a597808d18aef
Edwin3002 <vargas11742@gmail.com>
2022-09-30 20:30:59 -0500

    img nosotros

--- src/pages/contacto.js
+++ src/pages/contacto.js
@@ -6,11 +6,11 @@ const contacto = () => {
       <form className='w-4/5 mx-auto my-8'>
         <div className="mb-6">
           <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Correo</label>
-          <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@example.com" required="" />
+          <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-300 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@example.com" required="" />
         </div>
         <div className="mb-6">
           <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Mensaje</label>
-          <textarea type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+          <textarea type="password" id="password" className="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-300 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
         </div>
         <button type="submit" className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-400 dark:hover:bg-red-500 dark:focus:ring-blue-800">Submit</button>
       </form>
--- src/pages/nosotros.js
+++ src/pages/nosotros.js
@@ -7,7 +7,9 @@ const nosotros = () => {
       <p className='m-8 w-4/5 md:w-3/4 text-justify text-xl md:text-2xl mx-auto'>Somos una academia de artes con programas que desarrollan las habilidades artísticas de los estudiantes, enfocados en sus intereses para contribuir más a su formación, con el fin de potenciar su creatividad y enfoque. En la academia contamos con múltiples instrumentos como guitarra, batería, guitarra eléctrica, bajo y teclado principalmente, sin embargo puedes traer tu propio instrumento. Así mismo contamos con excelentes docentes y alumnos con un gran amor por la música, además de ser buenos seres humanos con gusto por la enseñanza tanto a docentes como a alumnos.</p>
       <h2 className='text-3xl md:text-5xl  text-center font-bold mt-4 text-red-400'>Porqué nosotros?</h2>
       <p className='m-8 w-4/5 md:w-3/4 text-justify text-xl md:text-2xl mx-auto'>Porque no somos solo una academia, es un hogar donde puedes expresar tus emociones a través de la música, un hogar agradable con gente de diferentes gustos, un hogar donde puedes aprender a tu ritmo y sin presiones. Además, valoramos mucho a las personas que dan su compromiso, respeto, confianza y paz en nosotros. Entonces, de nuestro lado sentirás esos valores y también de la gente que nos rodea en la academia.</p>
-
+      <figure className='flex w-3/4 mx-auto mb-4'>
+        <img className='flex  w-1/2 mx-auto' src='https://res.cloudinary.com/edwin3002/image/upload/v1664587555/Erato/logo-erato_ohm0ri.png' />
+      </figure>
     </div>
   )
 }
